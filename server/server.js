1df71c64731177648178c6b17df0019aadd53b70
firstjob <kaoutarhilali933@gmail.com>
2026-02-15 00:46:48 +0100

    feat: implement JSON message handling and structured real-time broadcast

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ console.log("WebSocket server running on ws://localhost:" + PORT);
 function broadcast(message) {
   clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(message);
+      client.send(JSON.stringify(message));
     }
   });
 }
@@ -22,8 +22,27 @@ wss.on("connection", (socket) => {
 
   clients.push(socket);
 
+  socket.on("message", (data) => {
+    try {
+      const message = JSON.parse(data);
+
+      console.log("Message reçu :", message);
+
+      if (message.type === "TABLE_RESERVED") {
+        broadcast({
+          type: "TABLE_RESERVED",
+          tableId: message.tableId,
+        });
+      }
+
+    } catch (error) {
+      console.log("Invalid JSON message");
+    }
+  });
+
   socket.on("close", () => {
     console.log("Client disconnected");
+
     const index = clients.indexOf(socket);
     if (index !== -1) {
       clients.splice(index, 1);
