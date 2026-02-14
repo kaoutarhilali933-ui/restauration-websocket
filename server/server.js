a06df56f864ac6a34a43d2aae4b63484d5701ed6
firstjob <kaoutarhilali933@gmail.com>
2026-02-15 00:42:52 +0100

    feat: handle client connection and disconnection

--- server/server.js
+++ server/server.js
@@ -5,3 +5,11 @@ const PORT = 3000;
 const wss = new WebSocket.Server({ port: PORT });
 
 console.log("WebSocket server running on ws://localhost:" + PORT);
+
+wss.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
