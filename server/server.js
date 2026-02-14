42a627f2e06fd52df0ef488eae6c0bcfb8000d49
firstjob <kaoutarhilali933@gmail.com>
2026-02-15 00:44:51 +0100

    feat: store connected clients and implement broadcast function

--- server/server.js
+++ server/server.js
@@ -4,12 +4,29 @@ const PORT = 3000;
 
 const wss = new WebSocket.Server({ port: PORT });
 
+const clients = [];
+
 console.log("WebSocket server running on ws://localhost:" + PORT);
+
+// Fonction broadcast
+function broadcast(message) {
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+}
 
 wss.on("connection", (socket) => {
   console.log("Client connected");
 
+  clients.push(socket);
+
   socket.on("close", () => {
     console.log("Client disconnected");
+    const index = clients.indexOf(socket);
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
   });
 });
